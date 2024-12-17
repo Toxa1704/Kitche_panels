9f687d98a7acb4adf35a63cf610eac4caa9e7bc0
Toxa1704 <Toxa1704@outlook.com>
2024-12-17 11:11:33 +0200

    main

--- js/script.js
+++ js/script.js
@@ -1,1 +1,19 @@
-console.log("ok!!!");
+const prevButton = document.querySelector(".prev");
+const nextButton = document.querySelector(".next");
+const items = document.querySelectorAll(".block-card");
+let currentItem = 0;
+const totalSlides = document.querySelectorAll('.block-card').length;
+
+function showSlide(index) {
+  items[currentItem].classList.remove("active");
+  
+  currentItem = (index + items.length) % items.length;
+  items[currentItem].classList.add("active");
+}
+prevButton.addEventListener("click", () => {
+  showSlide(currentItem - 1);
+});
+nextButton.addEventListener("click", () => {
+  showSlide(currentItem + 1);
+});
+
