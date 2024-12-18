8cac4b30b9d7c7a24cc80c753fdf5fa3c0d2d763
Toxa1704 <Toxa1704@outlook.com>
2024-12-18 09:48:47 +0200

    main

--- js/script.js
+++ js/script.js
@@ -1,12 +1,17 @@
 const prevButton = document.querySelector(".prev");
 const nextButton = document.querySelector(".next");
 const items = document.querySelectorAll(".block-card");
+const burgMenu = document.querySelector(".header-burg-menu");
+const close = document.querySelector(".header-burg-close");
+const burgWrapper = document.querySelector(".header-burg-wrapper");
+const burgList = document.querySelector(".header-burg-list");
+const burgNav = document.querySelectorAll(".header-burg-nav>a");
 let currentItem = 0;
-const totalSlides = document.querySelectorAll('.block-card').length;
+const totalSlides = document.querySelectorAll(".block-card").length;
 
 function showSlide(index) {
   items[currentItem].classList.remove("active");
-  
+
   currentItem = (index + items.length) % items.length;
   items[currentItem].classList.add("active");
 }
@@ -17,3 +22,23 @@ nextButton.addEventListener("click", () => {
   showSlide(currentItem + 1);
 });
 
+burgMenu.addEventListener("click", () => {
+  if (burgList.style.display === "none" || burgList.style.display === "") {
+    burgList.style.display = "block";
+    burgWrapper.style.display = "none";
+  } else {
+    burgList.style.display = "none";
+    burgWrapper.style.display = "flex";
+  }
+});
+
+close.addEventListener("click", () => {
+  burgList.style.display = "none";
+  burgWrapper.style.display = "flex";
+});
+burgNav.forEach((link) => {
+  link.addEventListener("click", () => {
+    burgList.style.display = "none";
+    burgWrapper.style.display = "flex";
+  });
+});
